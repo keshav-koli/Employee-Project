4cd4f6b09b652a15c1a5be58c3ab81c97a05cff8
keshav-koli <keshavkoli5588@gmail.com>
2025-03-28 06:28:01 +0530

    add employee creation form and update database structure

--- src/Component/CreateEmp.jsx
+++ src/Component/CreateEmp.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const CreateEmp = () => {
-  return (
-    <div>CreateEmp</div>
-  )
-}
+  let [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    department: "",
+  });
 
-export default CreateEmp
+  const handleChanges = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:4000/employees", formData);
+
+    //after posting
+    setFormData({
+      name: "",
+      email: "",
+      department: "",
+    });
+  };
+  return (
+    <div>
+      <h1>Enter New Employee Data</h1>
+      <form onSubmit={handleForm}>
+        <h4>Enter the Name :</h4>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChanges}
+        ></input>
+        <h4>Enter the Email :</h4>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChanges}
+        ></input>
+        <h4>Enter the Department :</h4>
+        <input
+          type="text"
+          name="department"
+          value={formData.department}
+          onChange={handleChanges}
+        ></input>
+        <input type="submit" name="submit" value="submit"></input>
+      </form>
+    </div>
+  );
+};
+
+export default CreateEmp;
